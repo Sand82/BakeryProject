97d930384961a0dbd736d49a8bad2bea168788fe
Sand82 <sandoki@abv.bg>
2022-03-02 20:36:12 +0200

    adjust index home page

--- Bakery/Controllers/HomeController.cs
+++ Bakery/Controllers/HomeController.cs
@@ -7,24 +7,14 @@ using System.Diagnostics;
 namespace Bakery.Controllers
 {
     public class HomeController : Controller
-    {
-        private readonly IHostEnvironment env;
-        public HomeController(IHostEnvironment env)
-        {
-            this.env = env;
-        }
-
+    {     
         public IActionResult Index()
         {
-            var pathOne = env.ContentRootFileProvider.GetFileInfo("wwwroot/FirstPic.jpg")?.PhysicalPath;
-            var pathSecond = env.ContentRootFileProvider.GetFileInfo("SecondPic.jpg")?.PhysicalPath;
-            var pathThird = env.ContentRootFileProvider.GetFileInfo("ThirdPic.jpg")?.PhysicalPath;
-
             var indexModel = new IndexViewModel
             {
-                FirstPicture = pathOne,
-                SecondPicture = pathSecond,
-                ThirdPicture = pathThird,
+                FirstPicture = "./wwwroot/FirstPic.jpg",
+                SecondPicture = "./wwwroot/SecondPic.jpg",
+                ThirdPicture = "./wwwroot/ThirdPic.jpg",
             };
 
             return View(indexModel);
