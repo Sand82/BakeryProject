a332c78beff34ee34f51b01ec125f51a9c0abf0f
Sand82 <sandoki@abv.bg>
2022-03-14 06:43:23 +0200

    change small thinks on view template

--- Bakery/Data/DataConnectionString.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Bakery.Data
-{
-    public class DataConnectionString
-    {
-        public const string DataParameters =
-            @"Server = localhost; User Id = sa; Password = @Stefanov820605; Database = BackeryProject";
-    }
-}
