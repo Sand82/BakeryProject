5f345d1b8dc50e42d90eee4c60ddd1a2c9ca9191
Sand82 <sandoki@abv.bg>
2022-03-27 18:38:03 +0300

    add api edit controller

---
+++ Bakery/Controllers/EditController.cs
@@ -0,0 +1,19 @@
+﻿using Bakery.Models.EditItem;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Bakery.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EditItemController : Controller
+    {
+        [HttpPost]
+        [Authorize]
+        [IgnoreAntiforgeryToken]
+        public ActionResult Post(EditItemDataModel model) 
+        {
+            return RedirectToAction("");
+        }
+    }
+}
---
+++ Bakery/Models/EditItem/EditItemDataModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+using static Bakery.Data.Constants;
+
+namespace Bakery.Models.EditItem
+{
+    public class EditItemDataModel
+    {
+        public int ItemId { get; set; }
+
+        [Range(ItemMaxValue, ItemMinValue)]
+        public int Quantity { get; set; }
+    }
+}
