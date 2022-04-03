c09ba13fea9e045e6198a912d86a2750bd7f7246
Sand82 <sandoki@abv.bg>
2022-04-03 19:37:16 +0300

    Introduce new area task

---
+++ Bakery/Areas/Task/Controllers/OrganizerController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Bakery.Areas.Task.Controllers
+{
+    public class OrganizerController : Controller
+    {
+        public IActionResult Request() 
+        { 
+            return View(); 
+        }
+    }
+}
