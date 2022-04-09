40cd2a29f5202ef888dc8368db4cc070a76c464c
Sand82 <sandoki@abv.bg>
2022-04-09 14:59:32 +0300

    add some unit tests for order service

---
+++ Bakery.Tests/Services/OrderServiceTests.cs
@@ -0,0 +1,37 @@
+﻿using Bakery.Service;
+using Bakery.Tests.Mock;
+using Xunit;
+
+namespace Bakery.Tests.Services
+{
+    public class OrderServiceTests
+    {
+        [Fact]
+        public void CreatOrderShouldReturnCorrectResult()
+        {
+            using var data = DatabaseMock.Instance;
+
+            var orderService = new OrderService(data);
+
+            var userId = "Some id";
+
+            var result =  orderService.CreatOrder(userId);
+
+            Assert.NotNull(result);
+        }
+
+        [Fact]
+        public void CreatItemShouldReturnCorrectResult()
+        {
+            using var data = DatabaseMock.Instance;
+
+            var orderService = new OrderService(data);
+
+            var userId = "Some id";
+
+            var result = orderService.CreateItem(1, "Bread", 3.20m, 3, userId);
+
+            Assert.NotNull(result);
+        }
+    }
+}
