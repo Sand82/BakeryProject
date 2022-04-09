35843a54cfb7cda4502d7e41d503b3278393e345
Sand82 <sandoki@abv.bg>
2022-04-09 15:14:43 +0300

    add some new unit tests in order service

--- Bakery.Tests/Services/OrderServiceTests.cs
+++ Bakery.Tests/Services/OrderServiceTests.cs
@@ -1,5 +1,8 @@
-﻿using Bakery.Service;
+﻿using Bakery.Data.Models;
+using Bakery.Service;
 using Bakery.Tests.Mock;
+using System;
+using System.Collections.Generic;
 using Xunit;
 
 namespace Bakery.Tests.Services
@@ -15,7 +18,7 @@ namespace Bakery.Tests.Services
 
             var userId = "Some id";
 
-            var result =  orderService.CreatOrder(userId);
+            var result = orderService.CreatOrder(userId);
 
             Assert.NotNull(result);
         }
@@ -32,6 +35,92 @@ namespace Bakery.Tests.Services
             var result = orderService.CreateItem(1, "Bread", 3.20m, 3, userId);
 
             Assert.NotNull(result);
+        }
+
+        [Fact]
+        public void FindOrderByUserIdShouldReturnCorrectResult()
+        {
+            using var data = DatabaseMock.Instance;
+
+            var orders = CreateListOrders();
+
+            data.Orders.AddRange(orders);
+
+            data.SaveChanges();
+
+            var orderService = new OrderService(data);
+
+            var userId = "User2";
+
+            var result = orderService.FindOrderByUserId(userId);
+
+            Assert.NotNull(result);
+            Assert.Equal(userId, result.UserId);
+        }
+
+        [Fact]
+        public void FindOrderByUserIdShouldReturnZeroWhitIncorectMethodParameter()
+        {
+            using var data = DatabaseMock.Instance;
+
+            var orders = CreateListOrders();
+
+            data.Orders.AddRange(orders);
+
+            data.SaveChanges();
+
+            var orderService = new OrderService(data);
+
+            var userId = "OtherUser2";
+
+            var result = orderService.FindOrderByUserId(userId);
+
+            Assert.Null(result);
+            
+        }
+
+        private List<Order> CreateListOrders()
+        {
+            var orders = new List<Order>();
+
+            var items = CreateListItem();
+
+            for (int i = 1; i <= 5; i++)
+            {
+                var order = new Order
+                {
+                    Id = i,
+                    DateOfOrder = DateTime.UtcNow,
+                    IsFinished = false,
+                    Items = items,
+                    UserId = $"User{i}",
+                };
+
+                orders.Add(order);
+            }
+
+            return orders;
+        }
+
+        private List<Item> CreateListItem()
+        {
+            var items = new List<Item>();
+
+            for (int i = 1; i < 6; i++)
+            {
+                var item = new Item
+                {
+                    Id = i,
+                    ProductName = $"Bread{i}",
+                    ProductId = i,
+                    ProductPrice = 3.20m + i,
+                    Quantity = i,
+                };
+
+                items.Add(item);
+            }
+
+            return items;
         }
     }
 }
