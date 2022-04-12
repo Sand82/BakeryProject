57687e2555b3ab32df52e379164571183d613bae
Sand82 <sandoki@abv.bg>
2022-04-12 18:43:03 +0300

    add section styles and section scripts on some views

---
+++ Bakery.Tests/Controllers/OrganizerControllerTests.cs
@@ -0,0 +1,52 @@
+﻿using Bakery.Areas.Task.Controllers;
+using Bakery.Data;
+using Bakery.Service;
+using Bakery.Tests.Mock;
+
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Security.Claims;
+using Xunit;
+
+namespace Bakery.Tests.Controllers
+{
+    public class OrganizerControllerTests
+    {
+        [Fact]
+        public void RequestActionShoulReturnCorectResult()
+        {
+            using var data = DatabaseMock.Instance;
+
+            var date = DateTime.UtcNow;            
+
+            var controller = CreateClaimsPrincipal(data);
+
+            var result = controller.Request();
+
+            var viewResult = Assert.IsType<ViewResult>(result);
+
+            Assert.NotNull(result);
+        }
+
+        private OrganizerController CreateClaimsPrincipal(BakeryDbContext data)
+        {
+            var organizerService = new OrganizerService(data);
+
+            var user = new ClaimsPrincipal(new ClaimsIdentity(new Claim[]
+           {
+                new Claim(ClaimTypes.NameIdentifier, "vqra@abv.bg"),
+                new Claim(ClaimTypes.Name, "vqra@abv.bg")
+
+           }, "TestAuthentication"));
+
+            var controller = new OrganizerController(organizerService);
+
+            controller.ControllerContext = new ControllerContext();
+
+            controller.ControllerContext.HttpContext = new DefaultHttpContext { User = user };
+
+            return controller;
+        }
+    }
+}
