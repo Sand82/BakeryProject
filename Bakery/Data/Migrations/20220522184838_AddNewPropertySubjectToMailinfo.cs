dfe223287b9329db3501a48a8a5e5e26c21f2db1
Sand82 <sandoki@abv.bg>
2022-05-22 23:03:12 +0300

    in the way to implement sendgrid functionality

---
+++ Bakery/Data/Migrations/20220522184838_AddNewPropertySubjectToMailinfo.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Bakery.Data.Migrations
+{
+    public partial class AddNewPropertySubjectToMailinfo : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Subject",
+                table: "MailInfos",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Subject",
+                table: "MailInfos");
+        }
+    }
+}
